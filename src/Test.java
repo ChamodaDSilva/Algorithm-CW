67f135ccad6b38f9c7dd5a5dd73d249a0f04296f
Chamoda De Silva <chamoda12345678@gmail.com>
2022-03-15 09:22:16 +0530

    make project for AStar

--- src/Test.java
+++ src/Test.java
@@ -1,19 +1,45 @@
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Test {
     public static String[][] arr;
+    public static int si;
+    public static int sj;
+    public static int ei;
+    public static int ej;
+    public static int [][] blockList;
+
     public static void main(String[] args) throws IOException {
         File file=new File("maze30_5.txt");
         Scanner input=new Scanner(file);
         arr=new String[numOfRows()][numOfColumns()];
 
-        int index=0;
+        int index=0;//initialize
         while(input.hasNextLine()){
             arr[index]=input.nextLine().split("(?!^)");
             index++;
         }
         printArr();
+
+        ////////////
+        startAndEnd();
+        blocks();
+        System.out.println(si);
+        System.out.println(sj);
+        System.out.println(ei);
+        System.out.println(ej);
+        AStar aStar = new AStar(numOfColumns(),numOfRows(), si, sj, ei, ej,
+                blockList);
+
+        aStar.display();
+        aStar.process();
+        aStar.displayScores();
+        aStar.displaySolution();
+
+
+
+        ///////////////
 
 
     }
@@ -31,12 +57,41 @@ public class Test {
         numOfColumns=input.nextLine().split("(?!^)").length;
         return numOfColumns;
     }
+    public static void startAndEnd(){
+        for (int i=0;i<arr.length; i++) {
+            for (int j=0;j<arr[i].length;j++) {
+                if(arr[i][j].equals("S")){
+                    si=i;
+                    sj=j;
+                }else if(arr[i][j].equals("F")){
+                    ei=i;
+                    ej=j;
+                }
+            }
+        }
+    }
     public static void printArr(){
         for (String [] row:arr) {
             for (String column:row) {
                 System.out.print(column);
             }
             System.out.println();
+        }
+    }
+    public static void blocks(){
+        ArrayList <int[]> blocks = new ArrayList<int[]>();
+        for (int i=0;i<arr.length; i++) {
+            for (int j=0;j<arr[i].length;j++) {
+                if(arr[i][j].equals("0")){
+                    blocks.add(new int[]{i,j});
+                }
+            }
+        }
+
+        //initialize blockList array
+        blockList=new int[blocks.size()][2];
+        for(int i=0;i<blocks.size();i++){
+            blockList[i]=blocks.get(i);
         }
     }
 }
